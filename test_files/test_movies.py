97107824a2dc817a7078c0799baabf9b8ae1066f
jccli <jc.climentpardo@gmail.com>
2022-06-24 18:01:04 +0200

    test_file + data

--- test_files/test_movies.py
+++ test_files/test_movies.py
@@ -1,1 +1,21 @@
 # Test file to check a basic recommendation engine with some requests.#
+
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+import os
+
+print(os.getcwd())
+path_parent = os.path.dirname(os.getcwd())
+print(path_parent)
+os.chdir(path_parent)
+
+
+df = pd.read_csv('test_data/movies_metadata.csv', low_memory=False)
+
+# Select just relevant features
+relevant_features = ['title','genres', 'release_date', 'runtime', 'vote_average', 'vote_count']
+df = df[relevant_features]
+
+# Print the dataframe
+df.head()
