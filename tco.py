ddcba93f8a24e2393798a7b81f71a4bb072dc15b
Alvaro Ritter <alvaro.ritter@tum.de>
2022-07-08 17:24:26 +0200

    TCO Setup, added ADAC collection query

--- tco.py
+++ tco.py
@@ -1,1 +1,8 @@
-# In this file the TCO logic should be defined.
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://jayqwalin:sBizum7HyN2Isvmi@cluster0.hvkyyid.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["AkinaDB"]
+collection = db["listings_AutoScout"]
+
+
--- test_files/test_queries.py
+++ test_files/test_queries.py
@@ -3,9 +3,10 @@ from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://jayqwalin:sBizum7HyN2Isvmi@cluster0.hvkyyid.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["AkinaDB"]
-collection = db["listings_AutoScout"]
+listings = db["listings_AutoScout"]
+adac = db["adac_scores"]
 
-print(collection.find({"MAKENAME":"BMW"}))
+print(listings.find({"MAKENAME": "BMW"}))
 # results = collection.find({"MAKENAME":"BMW"}).pretty()
 results = db.inventory.find({"MAKENAME":"BMW"})
 
