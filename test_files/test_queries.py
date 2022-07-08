10286f5dd51073c193e2f3ff63392d7330d557b9
Alvaro Ritter <alvaro.ritter@tum.de>
2022-07-08 09:29:17 +0200

    Alvarito Files

---
+++ test_files/test_queries.py
@@ -0,0 +1,7 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://jayqwalin:sBizum7HyN2Isvmi@cluster0.hvkyyid.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["AkinaDB"]
+collection = db["listings_AutoScout"]
+
